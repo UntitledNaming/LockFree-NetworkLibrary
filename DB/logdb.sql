4f22175ccd2a6e8845214f3458d246f3a6c9dc08
UntitledNaming <117499155+UntitledNaming@users.noreply.github.com>
2025-11-01 16:43:19 +0900

    Create logdb.sql

---
+++ DB/logdb.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE `logdb`;
+
+
+CREATE TABLE `logdb`.`monitorlog` 
+(
+  `no` BIGINT NOT NULL AUTO_INCREMENT,
+  `logtime`		DATETIME,
+  `serverno`	INT NOT NULL,  -- 서버마다의 고유번호 각자 지정
+  `type`		INT NOT NULL,
+  `avr`		INT NOT NULL,
+  `min`		INT NOT NULL,
+  `max`		INT NOT NULL,
+  -- avr  추가
+  -- min  추가
+  -- max  추가
+  
+PRIMARY KEY (`no`) );
+
+
